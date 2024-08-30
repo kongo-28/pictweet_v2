c540f020a4e7bf769b6bb8542ccb3a7894bc7896
kongo-28 <ryota.tennis.ku@gmail.com>
2024-08-30 17:02:10 +0900

    ツイート保存時にユーザー情報も追加(merge)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,9 +34,9 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
   end
-
+  
   def set_tweet
     @tweet = Tweet.find(params[:id])
   end
